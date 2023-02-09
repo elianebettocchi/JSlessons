239888d1b5133e70d6edb2a3d40b504f08f653e4
Eliane <elianebettocchi@gmail.com>
2023-02-08 20:21:03 -0500

    push

---
+++ test.js
@@ -0,0 +1,16 @@
+function sum(x,y){
+    console.log(x+y)
+}
+sum(137659, 980543);
+function subtract(x,y){
+    console.log(x-y)
+}
+subtract(3452, 1267);
+function multiply(x,y){
+    console.log(x*y)
+}
+multiply(234, 678);
+function divide(x,y){
+    console.log(x/y)
+}
+divide(642, 4);
